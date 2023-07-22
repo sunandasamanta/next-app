867175aea0fe820002b9a09e09ff6d98ab0972b2
sunandasamanta <samantasunanda724@gmail.com>
2023-07-22 15:54:13 +0530

    primarily formed app

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 import Link from "next/link"
-import CreateAccount from "./register/page"
+import NursingHomeLandingPage from "./components/NursingHomeLandingPage"
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-400 to-white flex items-center justify-center">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        <Link href="/register/"> Join Us </Link>
-      </button>
+      <NursingHomeLandingPage />
     </div>
   )
 }
