2df7d6f301fe4dc825b4106d4242ad35e7fb7d93
sunandasamanta <samantasunanda724@gmail.com>
2023-07-22 23:16:02 +0530

    added padding to the image

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -10,7 +10,7 @@ export default function NotFound() {
         src="https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
         width={600} 
         height={300} 
-        className="rounded-md"
+        className="rounded-xl px-2"
         alt="puppy-error"
       />
       <p>
