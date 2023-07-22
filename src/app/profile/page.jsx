0d366c8468bebc04d044ffa3174c733db56f5093
sunandasamanta <samantasunanda724@gmail.com>
2023-07-22 17:19:17 +0530

    init profile page

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ProfilePage() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1
+        className='text-4xl font-bold mb-4'
+      >
+        Profile Page
+      </h1>
+    </div>
+  )
+}
