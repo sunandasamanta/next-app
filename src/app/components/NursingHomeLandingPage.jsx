74d2cdd1940883db3af24d9244a0a7b594d51828
sunandasamanta <samantasunanda724@gmail.com>
2023-07-24 23:59:46 +0530

    font and text color changed

--- src/app/components/NursingHomeLandingPage.jsx
+++ src/app/components/NursingHomeLandingPage.jsx
@@ -6,14 +6,14 @@ const NursingHomeLandingPage = () => {
   return (
     <div className="min-h-screen flex flex-col px-3 py-2">
       {/* <Navbar /> */}
-      <main className="flex flex-col items-center justify-center flex-grow text-blue-50">
-        <h1 className="text-4xl font-bold mb-4">Nursing Home Name</h1>
-        <h2 className="text-2xl font-semibold mb-2">A Good Heading</h2>
-        <p className="text-lg text-center max-w-md mb-8">
+      <main className="flex flex-col font-sans items-center justify-center flex-grow text-500">
+        <h1 className="text-5xl font-bold mb-4">Nursing Home Name</h1>
+        <h2 className="text-3xl font-semibold mb-2">A Good Heading</h2>
+        <p className="text-xl font-medium text-center max-w-md mb-8">
           A Catchy Tagline - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
           vestibulum gravida nisi, quis facilisis dolor mollis ac.
         </p>
-        <p className="text-lg text-center max-w-md">
+        <p className="text-lg font-normal text-center max-w-md text-white">
           An Effective Paragraph - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
           vestibulum gravida nisi, quis facilisis dolor mollis ac. Sed eu mauris felis. Donec
           consectetur urna in sapien cursus, vitae fermentum purus facilisis. Curabitur
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
-import Link from "next/link"
+// import Link from "next/link"
 import NursingHomeLandingPage from "./components/NursingHomeLandingPage"
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-700 to-blue-100 flex items-center justify-center">
-      
+    <div className="min-h-screen flex items-center justify-center bg-pic">      
       <NursingHomeLandingPage />
-      
     </div>
   )
 }
